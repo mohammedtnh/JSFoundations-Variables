bdfb72d76f253a67cb315fa149adce4de47dbc53
Mohammed Hamdan <mohammed.tnh@gmail.com>
2021-01-10 15:17:51 +0300

    Done

---
+++ story.js
@@ -0,0 +1,6 @@
+const name = 'Hamdan';
+const age = 24;
+const hobby = 'Programming';
+
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!
+`);
